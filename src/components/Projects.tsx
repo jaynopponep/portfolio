204e2eb95f47611b286c978308563a398010de36
jaynopponep <noppone.p@gmail.com>
2024-08-15 13:52:52 -0400

    added Dovelop AI project

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -15,17 +15,19 @@ export default function Projects() {
                 Projects
             </div>
             <div className="flex justify-center flex-wrap mt-[20px] ">
-                <div className="w-[325px] bg-[#f0f0f0] border-r-[8px] overflow-hidden m-[20px] shadow-customShadow flex flex-col"> {/* Added 'flex flex-col' */}
+                <div
+                    className="w-[325px] bg-[#f0f0f0] border-r-[8px] overflow-hidden m-[20px] shadow-customShadow flex flex-col"> {}
                     <img src="/schedule_snake.gif" className="w-[100%] h-[200px]" alt="Schedule Snake"/>
-                    <div className="p-[16px] flex-grow"> {/* Added 'flex-grow' */}
+                    <div className="p-[16px] flex-grow"> {}
                         <h3 className="text-[28px] mb-[8px]">Schedule Snake</h3>
                         <p className="text-gray-600 text-[15px] leading-tight font-arsmaquette">
-                            A web application built to ensure a smooth enrollment process into university courses.
+                            A web application built with <strong>Next.JS</strong> and <strong>SQL</strong> to ensure a smooth enrollment process into university courses.
                             Professors create courses on Schedule Snake swiftly, and students are able to enroll
                             into the corresponding courses.
                         </p>
-                    </div> {/* End of 'flex-grow' section */}
-                    <div className="flex items-center justify-between mt-auto"> {/* Added 'mt-auto' */}
+                    </div>
+                    {/* End of 'flex-grow' section */}
+                    <div className="flex items-center justify-between mt-auto"> {}
                         <a href="https://github.com/jaynopponep/schedule-snake" target="_blank"
                            className="ml-2 inline-block px-[8px] py-[8px] bg-gray-800 text-white no-underline rounded text-sm">
                             <IoLogoGithub style={{fontSize: '36px'}}/>
@@ -38,18 +40,20 @@ export default function Projects() {
                         </div>
                     </div>
                 </div>
-                <div className="w-[325px] bg-[#f0f0f0] border-r-[8px] overflow-hidden m-[20px] shadow-customShadow flex flex-col"> {/* Added 'flex flex-col' */}
-                    <img src="/cliweather.gif" className="w-[100%] h-[200px]" alt="CLI Weather"/>
-                    <div className="p-[16px] flex-grow"> {/* Added 'flex-grow' */}
-                        <h3 className="text-[28px] mb-[8px]">CLI Weather App</h3>
+                <div
+                    className="w-[325px] bg-[#f0f0f0] border-r-[8px] overflow-hidden m-[20px] shadow-customShadow flex flex-col"> {}
+                    <img src="/dovelop-cli.gif" className="w-[100%] h-[200px]" alt="Dovelop AI"/>
+                    <div className="p-[16px] flex-grow"> {}
+                        <h3 className="text-[28px] mb-[8px]">Dovelop AI</h3>
                         <p className="text-gray-600 text-[15px] leading-tight font-arsmaquette">
-                            A CLI weather application built with Golang to check the weather of a specific zip code or
-                            region.
-                            Can be run from any directory a developer or user would be working on.
+                            A CLI to do list application made to improve productivity for developers and other users.
+                            Be able to track your to do list and edit them in real time while you are working on a project!
+                            Dovelop is powered by <strong>Go</strong>, uses <strong>OpenAI</strong> for task breakdowns, and local JSON data to store to do list data
                         </p>
-                    </div> {/* End of 'flex-grow' section */}
-                    <div className="flex items-center justify-between mt-auto"> {/* Added 'mt-auto' */}
-                        <a href="https://github.com/jaynopponep/cli-weather" target="_blank"
+                    </div>
+                    {}
+                    <div className="flex items-center justify-between mt-auto"> {}
+                        <a href="https://github.com/jaynopponep/dovelop-CLI" target="_blank"
                            className="ml-2 inline-block px-[8px] py-[8px] bg-gray-800 text-white no-underline rounded text-sm">
                             <IoLogoGithub style={{fontSize: '36px'}}/>
                         </a>
@@ -59,16 +63,17 @@ export default function Projects() {
                     </div>
                 </div>
                 <div
-                    className="w-[325px] bg-[#f0f0f0] border-r-[8px] overflow-hidden m-[20px] shadow-customShadow flex flex-col"> {/* Added 'flex flex-col' */}
+                    className="w-[325px] bg-[#f0f0f0] border-r-[8px] overflow-hidden m-[20px] shadow-customShadow flex flex-col"> {}
                     <img src="/byte_website.gif" className="w-[100%] h-[200px]" alt="BYTE Website"/>
-                    <div className="p-[16px] flex-grow"> {/* Added 'flex-grow' */}
+                    <div className="p-[16px] flex-grow"> {}
                         <h3 className="text-[28px] mb-[8px]">BYTE Club Website</h3>
                         <p className="text-gray-600 text-[15px] leading-tight font-arsmaquette">
-                            A website built with NextJS to showcase CCNY's BYTE Club website, its projects, and its
+                            A website built with Next.JS to showcase CCNY's BYTE Club website, its projects, and its
                             eboard.
                         </p>
-                    </div> {/* End of 'flex-grow' section */}
-                    <div className="flex items-center justify-between mt-auto"> {/* Added 'mt-auto' */}
+                    </div>
+                    {}
+                    <div className="flex items-center justify-between mt-auto"> {}
                         <div className="flex items-center justify-between mt-auto mb-[10px]">
                             <a href="https://github.com/BYTE-Club-CCNY/byte" target="_blank"
                                className="ml-2 inline-block px-[8px] py-[8px] bg-gray-800 text-white no-underline rounded text-sm">
@@ -85,18 +90,20 @@ export default function Projects() {
                         </div>
                     </div>
                 </div>
-                <div className="w-[325px] bg-[#f0f0f0] border-r-[8px] overflow-hidden m-[20px] shadow-customShadow flex flex-col"> {/* Added 'flex flex-col' */}
+                <div
+                    className="w-[325px] bg-[#f0f0f0] border-r-[8px] overflow-hidden m-[20px] shadow-customShadow flex flex-col"> {}
                     <img src="/fivebhais.gif" className="w-[100%] h-[200px]" alt="Five Bhais"/>
-                    <div className="p-[16px] flex-grow"> {/* Added 'flex-grow' */}
+                    <div className="p-[16px] flex-grow"> {}
                         <h3 className="text-[28px] mb-[8px]">Five Bhais Restaurant</h3>
                         <p className="text-gray-600 text-[15px] leading-tight font-arsmaquette">
                             A project built with other engineers demonstrating an online restaurant system where users
                             are able to
                             order takeout or delivery, and make dinner reservations after shopping on the menu. Built
-                            with NextJS, Flask, and MongoDB Atlas.
+                            with <strong>NextJS</strong>, <strong>Flask</strong>, and <strong>MongoDB Atlas</strong>.
                         </p>
-                    </div> {/* End of 'flex-grow' section */}
-                    <div className="flex items-center justify-between mt-auto"> {/* Added 'mt-auto' */}
+                    </div>
+                    {}
+                    <div className="flex items-center justify-between mt-auto"> {}
                         <a href="https://github.com/jaynopponep/five-bhais" target="_blank"
                            className="ml-2 inline-block px-[8px] py-[8px] bg-gray-800 text-white no-underline rounded text-sm">
                             <IoLogoGithub style={{fontSize: '36px'}}/>
