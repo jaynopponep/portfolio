37ac274f7286d859041e5f55d9b849ed5b9b21d8
jaynopponep <noppone.p@gmail.com>
2025-03-05 16:57:47 -0500

    added new projects and corresponding logos, changed font of 'PROJECTS'

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,11 +1,12 @@
 import React from "react";
-import { IoLogoGithub, IoLogoJavascript } from "react-icons/io5";
-import { FaExternalLinkAlt } from "react-icons/fa";
+import { IoLogoGithub, IoLogoJavascript, IoLogoVercel } from "react-icons/io5";
+import { FaExternalLinkAlt, FaDiscord } from "react-icons/fa";
 import { FaGolang } from "react-icons/fa6";
 import { SiNextdotjs, SiPython } from "react-icons/si";
+import { SiAmazondynamodb, SiLangchain, SiOpenai } from "react-icons/si";
 import { RiTailwindCssFill } from "react-icons/ri";
 import { DiMysql } from "react-icons/di";
-import { SiMongodb } from "react-icons/si";
+import { SiMongodb, SiGodaddy, SiGooglegemini } from "react-icons/si";
 
 export default function Projects() {
   return (
@@ -13,19 +14,95 @@ export default function Projects() {
       id="Projects"
       className="flex flex-col items-center justify-center font-extrabold text-[70px]"
     >
-      <div>PROJECTS</div>
+      <div className="text-5xl font-extrabold text-center text-gray-900 mb-12">PROJECTS</div>
       <div className="flex justify-center flex-wrap">
         <div className="w-[325px] bg-[#f0f0f0] border-r-[8px] overflow-hidden m-[20px] shadow-customShadow flex flex-col">
           {" "}
           {}
-          <img
-            src="/schedule_snake.gif"
-            className="w-[100%] h-[200px]"
-            alt="Schedule Snake"
-          />
+          <a href="https://spoileralert.nyc/">
+            <img
+                src="/SPOILERALERT.gif"
+                className="w-[325px] h-[200px]"
+                alt="Spoiler Alert"
+            />
+          </a>
           <div className="p-[16px] flex-grow">
             {" "}
             {}
+            <h3 className="text-[28px] mb-[8px]">Spoiler Alert</h3>
+            <p className="text-gray-600 text-[15px] leading-tight font-arsmaquette">
+              Spoiler Alert is a full-stack application where given an image or a search query,
+              retrieves data from the FDA API of any active recalls that are currently being investigated!
+              Simply take a picture of the product, or type in the name of the product, and it will instantly
+              retrieve the data.
+            </p>
+          </div>
+          {}
+          <div className="flex items-center justify-between mt-auto">
+            {" "}
+            {}
+            <a
+                href="https://github.com/BaljinderHothi/SpoilerAlert"
+                target="_blank"
+                className="ml-2 inline-block px-[8px] py-[8px] bg-gray-800 text-white no-underline rounded text-sm"
+            >
+              <IoLogoGithub style={{ fontSize: "36px" }} />
+            </a>
+            <div className="flex flex-row space-x-[2.5px] mr-2">
+              <SiNextdotjs className="w-[32px]" />
+              <IoLogoVercel className="w-[32px]" />
+              <SiMongodb className="w-[32px]" />
+              <SiGodaddy className="w-[32px]" />
+              <SiGooglegemini className="w-[32px]" />
+            </div>
+          </div>
+        </div>
+        <div className="w-[325px] bg-[#f0f0f0] border-r-[8px] overflow-hidden m-[20px] shadow-customShadow flex flex-col">
+          {" "}
+          {}
+          <a href="https://discord.com/oauth2/authorize?client_id=1299060600860839957&permissions=8&integration_type=0&scope=bot">
+            <img
+                src="/Silvercord.gif"
+                className="w-[325px] h-[200px]"
+                alt="Silvercord"
+            />
+          </a>
+          <div className="p-[16px] flex-grow">
+            {" "}
+            {}
+            <h3 className="text-[28px] mb-[8px]">Silvercord</h3>
+            <p className="text-gray-600 text-[15px] leading-tight font-arsmaquette">
+              Silvercord is a Discord bot, an assistant for supporting you in enrolling into future
+              courses, with seasonal LeetCode competitions to give an extra push to submit LeetCode problems
+              to pump up your high score to your Discord server's leaderboard!
+            </p>
+          </div>
+          {}
+          <div className="flex items-center justify-between mt-auto">
+            {" "}
+            {}
+            <a
+                href="https://github.com/BYTE-Club-CCNY/Silvercord"
+                target="_blank"
+                className="ml-2 inline-block px-[8px] py-[8px] bg-gray-800 text-white no-underline rounded text-sm"
+            >
+              <IoLogoGithub style={{ fontSize: "36px" }} />
+            </a>
+            <div className="flex flex-row space-x-[2.5px] mr-2">
+              <FaDiscord className="w-[40px]"/>
+              <SiLangchain className="w-[42px]"/>
+              <SiAmazondynamodb className="w-[32px]"/>
+              <FaGolang className="w-[32px]"/>
+            </div>
+          </div>
+        </div>
+        <div className="w-[325px] bg-[#f0f0f0] border-r-[8px] overflow-hidden m-[20px] shadow-customShadow flex flex-col">
+          <img
+            src="/schedule_snake.gif"
+            className="w-[325px] h-[200px]"
+            alt="Schedule Snake"
+          />
+          <div className="p-[16px] flex-grow">
             <h3 className="text-[28px] mb-[8px]">Schedule Snake</h3>
             <p className="text-gray-600 text-[15px] leading-tight font-arsmaquette">
               A web application built with <strong>Next.JS</strong> and{" "}
@@ -35,7 +112,6 @@ export default function Projects() {
               courses.
             </p>
           </div>
-          {/* End of 'flex-grow' section */}
           <div className="flex items-center justify-between mt-auto">
             {" "}
             {}
@@ -47,10 +123,9 @@ export default function Projects() {
               <IoLogoGithub style={{ fontSize: "36px" }} />
             </a>
             <div className="flex flex-row space-x-[2.5px] mr-2">
-              <SiNextdotjs className="w-[42px]" />
-              <IoLogoJavascript className="w-[42px]" />
-              <DiMysql className="w-[42px]" />
-              <RiTailwindCssFill className="w-[42px]" />
+              <SiNextdotjs className="w-[32px]" />
+              <DiMysql className="w-[32px]" />
+              <RiTailwindCssFill className="w-[32px]" />
             </div>
           </div>
         </div>
@@ -59,7 +134,7 @@ export default function Projects() {
           {}
           <img
             src="/dovelop-cli.gif"
-            className="w-[100%] h-[200px]"
+            className="w-[325px] h-[200px]"
             alt="Dovelop AI"
           />
           <div className="p-[16px] flex-grow">
@@ -86,7 +161,8 @@ export default function Projects() {
               <IoLogoGithub style={{ fontSize: "36px" }} />
             </a>
             <div className="flex flex-row  mr-2">
-              <FaGolang className="ml-auto" />
+              <SiOpenai className="w-[32px]" />
+              <FaGolang className="ml-auto w-[52px]" />
             </div>
           </div>
         </div>
@@ -95,7 +171,7 @@ export default function Projects() {
           {}
           <img
             src="/byte_website.gif"
-            className="w-[100%] h-[200px]"
+            className="w-[325px] h-[200px]"
             alt="BYTE Website"
           />
           <div className="p-[16px] flex-grow">
@@ -128,8 +204,8 @@ export default function Projects() {
               </a>
             </div>
             <div className="flex flex-row space-x-[6px] mr-2">
-              <SiNextdotjs className="w-[42px]" />
-              <RiTailwindCssFill className="w-[42px]" />
+              <SiNextdotjs className="w-[32px]" />
+              <RiTailwindCssFill className="w-[32px]" />
             </div>
           </div>
         </div>
@@ -138,7 +214,7 @@ export default function Projects() {
           {}
           <img
             src="/fivebhais.gif"
-            className="w-[100%] h-[200px]"
+            className="w-[325px] h-[200px]"
             alt="Five Bhais"
           />
           <div className="p-[16px] flex-grow">
@@ -165,11 +241,11 @@ export default function Projects() {
               <IoLogoGithub style={{ fontSize: "36px" }} />
             </a>
             <div className="flex flex-row space-x-[2px] mr-2">
-              <SiNextdotjs className="w-[40px]" />
-              <IoLogoJavascript className="w-[40px]" />
-              <SiMongodb className="w-[40px]" />
-              <SiPython className="w-[40px]" />
-              <RiTailwindCssFill className="w-[40px]" />
+              <SiNextdotjs className="w-[32px]" />
+              <IoLogoJavascript className="w-[32px]" />
+              <SiMongodb className="w-[32px]" />
+              <SiPython className="w-[32px]" />
+              <RiTailwindCssFill className="w-[32px]" />
             </div>
           </div>
         </div>
